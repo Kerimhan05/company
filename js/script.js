42b311ac278b12462f4d081ca588e69d27b58638
Kerimhan05 <ramazanovkerimhan05@gmail.com>
2022-02-14 03:28:17 +0300

    final layout

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// import Swiper from 'https://unpkg.com/swiper@8/swiper-bundle.esm.browser.min.js'
+
+const swiper = new Swiper('.swiper', {
+    // Navigation arrows
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+});
